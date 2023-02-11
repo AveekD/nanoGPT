d59512ad4ed3ea6a249f5a3bc315ec4cf625901a
aveekdg <aveekdg.umich.edu>
2023-02-11 02:27:24 -0500

    added new changes

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -5,49 +5,27 @@ Will save train.bin, val.bin containing the ids, and meta.pkl containing the
 encoder and decoder and some other related info.
 """
 import os
-import pickle
+import tiktoken
 import requests
 import numpy as np
 
 # download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'parsed.txt')
-# if not os.path.exists(input_file_path):
-#     data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-#     with open(input_file_path, 'w') as f:
-#         f.write(requests.get(data_url).text)
+input_file_path = os.path.join(os.path.dirname(__file__), 'train.txt')
+input_val_path = os.path.join(os.path.dirname(__file__), 'valid.txt')
 
 with open(input_file_path, 'r') as f:
     data = f.read()
-with open(input_file_path, 'r') as f:
-    data_lines = f.readlines()
+with open(input_val_path, 'r') as f:
+    data_valid = f.read()
 print(f"length of dataset in characters: {len(data):,}")
 
-# get all the unique characters that occur in this text
-chars = sorted(list(set(data)))
-vocab_size = len(chars)
-print("all the unique characters:", ''.join(chars))
-print(f"vocab size: {vocab_size:,}")
+train_data = data
+val_data = data_valid
 
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-def encode(s):
-    return [stoi[c] for c in s] # encoder: take a string, output a list of integers
-def decode(l):
-    ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-# create the train and test splits
-chapters = {}
-for line in data_lines:
-    print(line)
-
-n = len(data)
-train_data = data[:int(n*0.9)]
-val_data = data[int(n*0.9):]
-
-# encode both to integers
-train_ids = encode(train_data)
-val_ids = encode(val_data)
+# encode with tiktoken gpt2 bpe
+enc = tiktoken.get_encoding("gpt2")
+train_ids = enc.encode_ordinary(train_data)
+val_ids = enc.encode_ordinary(val_data)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
@@ -57,18 +35,6 @@ val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
-# save the meta information as well, to help us encode/decode later
-meta = {
-    'vocab_size': vocab_size,
-    'itos': itos,
-    'stoi': stoi,
-}
-with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
-    pickle.dump(meta, f)
+# train.bin has 301,966 tokens
+# val.bin has 36,059 tokens
 
-# length of dataset in characters:  1115394
-# all the unique characters:
-#  !$&',-.3:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
-# vocab size: 65
-# train has 1003854 tokens
-# val has 111540 tokens
